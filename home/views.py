e7b006f5098588bf51806402bf86e6509bf2e536
GazzaJ <johnge71@gmail.com>
2021-05-01 13:53:13 +0000

    create products template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+# Create your views here.
+
+
 def index(request):
     """A view to render the index.html page"""
+
     return render(request, 'home/index.html')
