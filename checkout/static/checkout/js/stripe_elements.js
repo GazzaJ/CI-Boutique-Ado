f76710f0685b007ceaea56cb519729fa13392b7f
GazzaJ <johnge71@gmail.com>
2021-05-08 15:43:05 +0000

    Added basic checkout functionality

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,79 @@
+/*
+    Core logic /payment flow for this app comes from:
+    https://stripe.com/docs/payments/save-during-payment
+    CSS from here:
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+
+var style = {
+    base: {
+        color: "#000",
+        fontFamily: '"Helvetica Neue", "Helvetica", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
+
+// Handle realtime validation errors on the card element
+card.AddEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent='';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form')
+
+form.AddEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        },
+        setup_future_usage: 'off_session'
+    }).then(function (result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({
+                'disabled': false
+            });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.onsubmit();
+            }
+        }
+    });
+});
