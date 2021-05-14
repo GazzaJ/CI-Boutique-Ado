bdc3c3f0dc1ad3012f54d8da354bbd525e378931
GazzaJ <johnge71@gmail.com>
2021-05-14 12:05:31 +0000

    Check webhook files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,7 +182,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ci-boutique-ado-app'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -204,7 +204,7 @@ if 'USE_AWS' in os.environ:
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'usd'
+STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
